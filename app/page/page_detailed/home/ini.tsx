51f8f28b0b5b92d5763adf1c9221f12e86ac6f65
toopique <sossmeed@gmail.com>
2025-07-01 23:58:23 +0700

    mulai home (lanyard)

--- app/page/page_detailed/home/ini.tsx
+++ app/page/page_detailed/home/ini.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import styles from "./ini.module.css";
+
+import Lanyard from "@/app/components/Lanyard/Lanyard";
 
 export default function Home() {
   return (
-    <section id="home" className={styles.container}>
+    <section id="home" className={styles.container}>        
       <h1 className={styles.title}>Home</h1>
+      <Lanyard />
       <p className={styles.subtitle}>
         Ini adalah halaman default menggunakan TypeScript dan CSS terpisah.
       </p>
